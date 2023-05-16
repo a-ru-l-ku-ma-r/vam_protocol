be1d5eb796a508d61ea5ba23a96950755d34e949
Arulkumar <132215718+a-ru-l-ku-ma-r@users.noreply.github.com>
2023-05-17 00:08:11 +0530

    Update a_proto.h

--- a_proto.h
+++ a_proto.h
@@ -1,1 +1,28 @@
 
+#ifndef __PROTO_H__
+#define __PROTO_H__
+
+/******************************************************/
+/*******************Include Files**********************/
+/******************************************************/
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+/*
+ * The structure describes the proto communication 
+ * structure holding configuration setting for the
+ * peripheral
+ */
+typedef struct{
+    uint32_t tx_pin_no;
+    uint32_t rx_pin_no;
+    uint32_t data_rate;
+}proto_commn_peripheral_t;
+
+
+
+
+#endif	/*  __PROTO_H__  */
