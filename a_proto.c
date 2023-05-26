65ec47a7969b8a3008be9923736a333080d0f0c7
Arulkumar <132215718+a-ru-l-ku-ma-r@users.noreply.github.com>
2023-05-26 23:24:54 +0530

    Update a_proto.c

--- a_proto.c
+++ a_proto.c
@@ -6,3 +6,16 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "a_proto.h"
+
+
+int sum_of_string(uint8_t *str)
+{
+  int sum_t = 0;
+  
+  while(*str){
+    sum += str;
+    str++;
+  }
+  return sum_t;
+}
+  
