a85f45646054301718b52b8dd873292213eaf278
Arulkumar <132215718+a-ru-l-ku-ma-r@users.noreply.github.com>
2024-07-04 22:16:18 +0530

    Update a_proto.c

--- a_proto.c
+++ a_proto.c
@@ -18,7 +18,19 @@ int sum_of_string(uint8_t *str)
   }
   return sum_t;
 }
-  
+
+int checksum_cal(int *_data_, int length)
+{
+  char loop_i;
+
+  int checksum;
+
+  for(loop_i = 0; loop_i < length ; loop_i++)
+    {
+      checksum =~  _data_[loop_i];
+    }
+}
+
 void transmit(uint8_t value)
 {
   
